aa993af606960c6d788792822b13782a892006a0
mukhammadqodir-alijonov <mukhammadqodir-alijonov.alijonov0608@gmail.com>
2023-04-30 23:16:31 +0500

    week2 is done

---
+++ OnlyCore/enum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        /* // 1
+         HaftaKunlari haftakuni = HaftaKunlari.Chorshanba;
+         if (haftakuni == HaftaKunlari.Yakshanba)
+         {
+             Console.WriteLine("Bugun dam");
+         }
+         else if (haftakuni == HaftaKunlari.Dushanba)
+         {
+             Console.WriteLine("Yana o'qish");
+         }
+         else Console.WriteLine("Dam yo'q");*/
+    }
+    /* //1public enum HaftaKunlari
+    {
+        Dushanba, Seshanba, Chorshanba, 
+        Payshanba, Juma, Shanba, Yakshanba
+    }*/
+    public static void Run(Fasl fasl)
+    {
+        if (fasl == Fasl.Bahor) Console.WriteLine("Bahor keldi");
+        else if (fasl == Fasl.Yoz) Console.WriteLine("Yoz keldi");
+        else Console.WriteLine("Fasil yo'q");
+    }
+}
+public enum Fasl
+{
+    Bahor, Yoz, Kuz, Qish
+}
--- OnlyCore/essential/Program.cs
+++ OnlyCore/essential/Program.cs
@@ -29,6 +29,7 @@ namespace essential
                 }
             }*/
 
+
             Ofitsant.Shout();
         }
 
