69bf67e849a8c5025ab99fe4e2e9547e14d5a708
mukhammadqodir-alijonov <mukhammadqodir-alijonov.alijonov0608@gmail.com>
2023-05-15 12:57:50 +0500

    working with json is partialy done

---
+++ OnlyCore/json/Models/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace json.Models;
+
+public class Currency
+{
+    public string Title { get; set; } = string.Empty;
+    public string Code { get; set; } = string.Empty;
+    public double cb_price { get; set; }
+    public string date { get; set; } = string.Empty;
+}
---
+++ OnlyCore/json/Program.cs
@@ -0,0 +1,73 @@
+﻿using json.Models;
+using Newtonsoft.Json;
+
+namespace json;
+
+public class Program
+{
+    public static Currency Find(string code)
+    {
+        foreach (var currency in Currencies)
+            if(code == currency.Code)
+                return currency;
+        return new Currency();
+    }
+    public static double UzsToUsd(double uzs)
+    {
+        var currency = Find("USD");
+        return  uzs/currency.cb_price;
+    }
+    private static List<Currency> Currencies = new List<Currency>();
+    static void Main(string[] args)
+    {
+        string path = "D:\\dataDemo\\dataDemo.json";
+        string json = File.ReadAllText(path);
+        Currencies = JsonConvert.DeserializeObject<List<Currency>>(json);
+        Console.WriteLine("'Dollir qilmoqchi bo'lgan summagizni kiriting = ");
+        double uzs = double.Parse(Console.ReadLine());
+        double usd = UzsToUsd(uzs);
+        Console.WriteLine(usd);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //User user = new User()
+        //{
+        //    Id = 1,
+        //    FirstName = "Muhammad",
+        //    LastName = "Alijonov",
+        //    Email = "Alijonov@gmail.com",
+        //    Password = "admin"
+        //};
+
+        //var json = JsonConvert.SerializeObject(users);
+        //File.WriteAllText(path, json.ToString());
+
+        //var result = File.ReadAllText(path);
+        //var json = JsonConvert.DeserializeObject<List<User>>(result);
+        //foreach (var user in json!)
+        //{
+        //    Console.WriteLine(user.FirstName + " " + user.LastName);
+        //}
+
+    }
+}
