edfb915ccdbcc1e4f6637e892200bfd60cb8b39d
mukhammadqodir-alijonov <mukhammadqodir-alijonov.alijonov0608@gmail.com>
2023-05-15 23:17:03 +0500

    delegate is parially done

---
+++ OnlyCore/delegate/Program.cs
@@ -0,0 +1,33 @@
+﻿using Models;
+
+namespace learningdelegate;
+public class Program
+{
+    public delegate bool Sender(string message);
+   
+    public static void Main(string[] args)
+    {
+        Sender sender = SendToInstagram;
+        sender("bla bla barga gomi");
+    }
+    public static bool SendToInstagram(string message)
+    {
+        Console.WriteLine($"{message} so'zi");
+        Console.WriteLine("Instagramdan keldi");
+        return true;
+    }
+    public static bool SendToTelegram(string message)
+    {
+        Console.WriteLine($"{message} so'zi");
+        Console.WriteLine("telegramdan keldi");
+        return true;
+    }
+
+    /*    //tuple
+        public static (bool isSucessful, string errorMessage) Run()
+        {
+            bool con = 5 < 4;
+            if (con) return (isSucessful: true, errorMessage: " ");
+            else return (isSucessful: false, errorMessage: "error bor ukam");
+        }*/
+}
