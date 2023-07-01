67e3ca130b321c820e4be5216859d9673dd533bb
mukhammadqodir-alijonov <mukhammadqodir-alijonov.alijonov0608@gmail.com>
2023-07-01 09:58:32 +0500

    get and getID

---
+++ AspNet web/asp-net-web-api/asp-net-web-api/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using asp_net_web_api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        List<User> listUser = new List<User>()
+        {
+            new User
+            {
+                Id = 1,
+                FirstName = "Alijonov",
+                LastName = "Muhammad"
+            },
+            new User
+            {
+                Id = 2,
+                FirstName = "Hamidov",
+                LastName = "Qutbiddin"
+
+            },
+            new User
+            {
+                Id = 3,
+                FirstName = "Alimov",
+                LastName = "Vali"
+            },
+            new User
+            {
+                Id = 4,
+                FirstName = "Xurramov",
+                LastName = "Qahhor"
+            },
+            new User
+            {
+                Id = 5,
+                FirstName = "Malikov",
+                LastName = "Sardor"
+            }
+        };
+        [HttpGet]
+        public List<User> GetUsers()
+        {
+            return listUser;
+        }
+        [HttpGet("id")]
+        public User GetId(int id)
+        {
+            return listUser.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
---
+++ AspNet web/asp-net-web-api/asp-net-web-api/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace asp_net_web_api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public  string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+    }
+}
