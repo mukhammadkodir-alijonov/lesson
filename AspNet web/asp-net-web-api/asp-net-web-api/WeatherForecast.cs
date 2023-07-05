080b7c750e398845db77e2352ecad5a54db77317
mukhammadkodir <98277057+mukhammadkodir-alijonov@users.noreply.github.com>
2023-07-05 15:18:14 +0500

    Update WeatherForecast.cs

--- AspNet web/asp-net-web-api/asp-net-web-api/WeatherForecast.cs
+++ AspNet web/asp-net-web-api/asp-net-web-api/WeatherForecast.cs
@@ -1,13 +1,1 @@
-namespace asp_net_web_api
-{
-    public class WeatherForecast
-    {
-        public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
-    }
-}
